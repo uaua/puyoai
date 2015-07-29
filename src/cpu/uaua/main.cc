56c76104127e2c266ed4eff0272bd58c19c1b953
uaua <uaua1ua@gmail.com>
2015-07-30 08:58:02 +0900

    nobasi

--- src/cpu/uaua/main.cc
+++ src/cpu/uaua/main.cc
@@ -22,6 +22,7 @@
 #include "core/client/ai/ai.h"
 #include "core/core_field.h"
 #include "core/frame_request.h"
+#include "core/sequence_generator.h"
 
 using namespace std;
 
@@ -64,38 +65,14 @@ public:
   struct Pack {
     int64_t score;
     int chains;
-    Pack() : score(0), chains(0) {}
-    Pack(int64_t score, int chains)
+    int hakkaY;
+    Pack() : score(0), chains(0), hakkaY(0) {}
+    Pack(int64_t score, int chains, int hakkaY)
         : score(score),
-          chains(chains) {
+          chains(chains),
+          hakkaY(hakkaY) {
     }
   };
-
-  Pack nobasiEval(const CoreField& pf) {
-    RensaResult res;
-    int hakkaY;
-    int64_t score = minScore;
-    const auto callback = [&](CoreField&& f, const ColumnPuyoList&) {
-      int tHakkaY = calcHakkaY(f);
-      RensaResult tRes = f.simulate();
-      // fprintf(stderr, "%d %d\n", tRes.chains, tHakkaY);
-      if (score < tRes.chains*100 + tHakkaY) {
-        score = tRes.chains*100 + tHakkaY;
-        res = tRes;
-        hakkaY = tHakkaY;
-      }
-    };
-    bool prohibits[FieldConstant::WIDTH]{};
-    RensaDetector::detectByDropStrategy(pf, prohibits, PurposeForFindingRensa::FOR_FIRE, 2, 13, callback);
-    // RensaDetector::detectSingle(pf, RensaDetectorStrategy::defaultDropStrategy(), callback);
-    
-    int sa = 0;
-    for (int x = 1; x <= FieldConstant::WIDTH-1; x++) {
-      sa += abs(pf.height(x)-pf.height(x+1));
-    }
-    auto con = calcCon(pf);
-    return Pack(res.chains*100 + hakkaY*3 - sa, res.chains);
-  }
 
   struct State {
     CoreField f;
@@ -103,15 +80,29 @@ public:
     Decision d;
     Pack p;
     bool dame;
-    State() : dame(false) {}
+    bool hakka;
+    State() : State({}, 0, {}) {}
     State(const CoreField& f, int turn, const Pack& pack)
         : State(f, turn, pack, false) {
     }
     State(const CoreField& f, int turn, const Pack& pack, bool dame)
-        : f(f), turn(turn), p(pack), dame(dame) {
+        : State(f, turn, pack, {}, dame) {
     }
+    State(const CoreField& f, int turn, const Pack& pack, const Decision& d, bool dame)
+        : f(f), turn(turn), d(d), p(pack), dame(dame), hakka(false) {
+    }
+
+    State next(const CoreField& f, const Pack& pack) {
+      State s(f, turn+1, pack, d, dame);
+      s.hakka = hakka;
+      return s;
+    }
+
     bool operator<(const State& e) const {
-      return p.score < e.p.score;
+      if (turn == e.turn) {
+        return p.score < e.p.score;
+      }
+      return turn > e.turn;
     }
   };
 
@@ -138,82 +129,144 @@ public:
     }
     return make_tuple(tate3, yoko3, lji);
   }
-  
-  DropDecision nobasi(const CoreField& f, const KumipuyoSeq& nexts,int depth) const {
-    priority_queue<State> q;
-    depth = nexts.size();
-    const int BEAM_WIDTH = 2000;
-    int lim[11]{};
-    int64_t best = minScore+1;
-    Decision d(3, 0);
-    State p;
-    q.push(State(f, 0, Pack(minScore, 0)));
 
-    //*
-    const auto& nobasiCallback = [&](const RefPlan& plan) {
+  typedef function<Pack (const CoreField&, const State& s)> Eval;
+
+  KumipuyoSeq genKumipuyoSequence(int k) const {
+    auto&& seq = generateRandomSequenceWithSeed(114514);
+    return seq.subsequence(3, k);
+  }
+
+  struct SearchResult {
+    Decision d;
+    int64_t score;
+    State state;
+    string msg;
+    SearchResult() {}
+    SearchResult(Decision d, int64_t score, const State& state, const string& msg)
+        : d(d), score(score), state(state), msg(msg) {}
+  };
+
+  SearchResult nobasiBeamSearch(const CoreField& f, const KumipuyoSeq& nexts, const Eval& eval) const {
+    priority_queue<State> q;
+    const int BEAM_WIDTH = 10;
+    int lim[111]{};
+    int64_t best = minScore+1;
+    int64_t dameBest = 0;
+    Decision d(3, 0);
+    State p, bp;
+    int depth = nexts.size();
+    string msg;
+
+    q.push(State(f, 0, Pack(minScore, 0, 0)));
+
+    const auto& nobasiCallback = [&p, &q, &eval](const RefPlan& plan) {
       if (plan.field().height(3) >= 12) return;
-      const Pack pack = nobasiEval(plan.field());
-      CoreField ff(f);
-      ff.simulate();
-      State s(ff, p.turn+1, pack);
+      const CoreField ff(plan.field());
+      const RensaResult& res = plan.rensaResult();
+      if (res.chains >= 10) {
+        // State s(ff, p.turn+1, p.p, p.d, true);
+        State s = p.next(ff, p.p);
+        if (p.turn == 0) {
+          s.hakka = plan.isRensaPlan();
+        }
+        q.push(s);
+        return;
+      }
+      const Pack pack = eval(ff, p);
+      State s = p.next(ff, pack);
       if (p.turn == 0) {
         s.d = plan.decisions().front();
-      } else {
-        s.d = p.d;
+        s.hakka = plan.isRensaPlan();
       }
       q.push(s);
     };
-    // */
-
-    /*
-    const auto& nobasiCallback = [&](const CoreField& f,
-                                     const std::vector<Decision>& d,
-                                     int numChigiri,
-                                     int framesToIgnite,
-                                     int lastDropFrames,
-                                     bool shouldFire) {
-      UNUSED_VARIABLE(numChigiri);
-      UNUSED_VARIABLE(framesToIgnite);
-      UNUSED_VARIABLE(lastDropFrames);
-      UNUSED_VARIABLE(shouldFire);
-      const int64_t score = nobasiEval(f);
-      CoreField ff(f);
-      ff.simulate();
-      State s(ff, p.turn+1, score);
-      if (p.turn == 0) {
-        s.d = d.front();
-      } else {
-        s.d = p.d;
-      }
-      q.push(s);
-    };
-    */
     
     while (!q.empty()) {
       // fprintf(stderr, "size: %zd\n", q.size());
       p = q.top();
       q.pop();
-      if (p.turn >= depth) {
-        // fprintf(stderr, "score: %" PRId64 "\n", p.score);
-        if (best < p.p.score) {
-          best = p.p.score;
-          d = p.d;
-        }
-        continue;
-      }
-      if (p.p.chains >= 9 || p.dame) {
-        State s(p.f, p.turn+1, p.p, true);
-        s.d = p.d;
-        q.push(s);
-        continue;
-      }
       if (lim[p.turn]++ >= BEAM_WIDTH) {
         continue;
       }
-      Plan::iterateAvailablePlans(p.f, nexts.subsequence(p.turn, 1), 1, nobasiCallback);
-      // Plan::iterateAvailablePlansWithoutFiring(f, nexts.subsequence(p.turn, 1), 2, nobasiCallback);
+      
+      if (p.turn >= depth) {
+        // fprintf(stderr, "score: %" PRId64 "\n", p.score);
+        // const Pack pack = eval(p.f, p);
+        if (best < p.p.score/*pack.score*/) {
+          bp = p;
+          msg = p.hakka ? "hakka!" : "nanimonashi";
+          best = p.p.score;
+          d = p.d;
+        }
+        if (p.dame) {
+          if (dameBest < p.p.score) {
+            dameBest = p.p.score;
+          }
+        }
+        continue;
+      }
+
+      if (p.dame) {
+        State s = p.next(p.f, p.p);
+        q.push(s);
+        continue;
+      }
+      
+      KumipuyoSeq seq;
+      if (p.turn < nexts.size()) {
+        seq = nexts.subsequence(p.turn, 1);
+      }
+      
+      Plan::iterateAvailablePlans(p.f, seq, 1, nobasiCallback);
     }
-    return DropDecision(d, "score: " + to_string(best));
+    
+    return SearchResult(d, best * 100000 + dameBest, bp, msg);
+  }
+  
+  DropDecision nobasi(const CoreField& f, const KumipuyoSeq& nexts,int depth) const {
+    UNUSED_VARIABLE(depth);
+    KumipuyoSeq s(nexts);
+    SearchResult res = nobasiBeamSearch(f, s, [this, &s](const CoreField& pf, const State& p) {
+        if (p.turn < s.size()) {
+          RensaResult res;
+          int hakkaY;
+          int64_t score = minScore;
+          const auto callback = [&score, &hakkaY, &res, this](CoreField&& f, const ColumnPuyoList&) {
+            int tHakkaY = calcHakkaY(f);
+            RensaResult tRes = f.simulate();
+            // fprintf(stderr, "%d %d\n", tRes.chains, tHakkaY);
+            if (score < tRes.chains*100 + tHakkaY) {
+              score = tRes.chains*100 + tHakkaY;
+              res = tRes;
+              hakkaY = tHakkaY;
+            }
+          };
+          bool prohibits[FieldConstant::WIDTH]{};
+          // RensaDetector::detectByDropStrategy(pf, prohibits, PurposeForFindingRensa::FOR_FIRE, 2, 13, callback);
+          RensaDetector::detectByFloatStrategy(pf, prohibits, 2, 13, callback);
+          
+          int sa = 0;
+          int ma = 0, mi = 114514;
+          for (int x = 1; x <= FieldConstant::WIDTH-1; x++) {
+            // sa += abs(pf.height(x)-pf.height(x+1));
+            ma = max(ma, pf.height(x));
+            mi = min(mi, pf.height(x));
+          }
+          sa = abs(ma-mi);
+          auto con = calcCon(pf);
+          // UNUSED_VARIABLE(con);
+          return Pack(res.chains*100 + hakkaY*2 - sa, res.chains, hakkaY);
+        } else {
+          // SearchResult nobasiBeamSearch(const CoreField& f, const KumipuyoSeq& nexts, const Eval& eval) const {
+        }
+      });
+    
+    return DropDecision(res.d,
+                        " score: " + to_string(res.score) +
+                        " chains: " + to_string(res.state.p.chains) +
+                        " hakka: " + to_string(res.state.p.hakkaY) +
+                        "\n" + res.msg);
   }
   
   DropDecision calc(int frameId,
