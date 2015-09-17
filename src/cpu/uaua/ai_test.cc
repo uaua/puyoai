094b53d7078b153116e48609a6090455ed6e1d91
uaua <uaua1ua@gmail.com>
2015-09-17 11:51:52 +0900

    Fix beamsearch

---
+++ src/cpu/uaua/ai_test.cc
@@ -0,0 +1,43 @@
+#include "ai.h"
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class AITest : public testing::Test {
+ protected:
+  int nobasiEval(const CoreField& f) {
+    char *argv[] = {
+      "./cpu/uaua/uaua",
+      "Player1"
+    };
+    UauaAI uaua(2, argv);
+    return uaua.nobasi(f);
+  }
+  DropDecision think(const CoreField& f) {
+    char *argv[] = {
+      "./cpu/uaua/uaua",
+      "Player1"
+    };
+    UauaAI uaua(2, argv);
+    const KumipuyoSeq& seq = KumipuyoSeqGenerator::generateRandomSequenceWithSeed(2, 1919);
+    return uaua.think(0, f, seq, {}, {}, false);
+  }
+};
+
+TEST_F(AITest, nobasiCheck)
+{
+    CoreField f;
+    int res = nobasiEval(f);
+    cout << res << endl;
+}
+
+TEST_F(AITest, thinkCheck)
+{
+  CoreField f;
+  DropDecision d = think(f);
+  (void)d;
+  cout << d.decision() << "\n" << d.message() << endl;
+}
